93434874c57a2ed8047a16aa9bf072a8eb32728b
Md. Naim Chowdhury <naimmcduck@gmail.com>
2020-10-04 07:57:54 +0200

    hash

--- classes/hash.php
+++ classes/hash.php
@@ -1,8 +1,12 @@
 <?php
 class Hash {
-    public static function make($string, $salt = '') {
-        return hash('sha256', $string . $salt);
+
+    public static function make($string){
+        return password_hash($string, PASSWORD_DEFAULT);
     }
+//    public static function make($string, $salt = '') {
+//        return hash('sha256', $string . $salt);
+//    }
 
     public static function salt($length) {
         return random_bytes($length);
