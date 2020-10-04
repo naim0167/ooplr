545ed6424de72d31fb01e5f50076f75f3fff4bf3
Md. Naim Chowdhury <naimmcduck@gmail.com>
2020-10-04 08:11:27 +0200

    login

--- classes/User.php
+++ classes/User.php
@@ -1,15 +1,48 @@
 <?php
 class User {
-    private $_db;
+    private $_db,
+            $_data,
+            $_sessionName;
 
     public function __construct($user = null){
         $this->_db = DB::getInstance();
+        $this->_sessionName = Config::get('session/session_name');
     }
 
-    public function create($fields = array()){
-        if(!$this->_db->insert('users', $fields)) {
+    public function create($field = array()){
+        if(!$this->_db->insert('users', $field)) {
             throw new Exception('There was a problem creating an account!');
         }
+    }
 
+    public function find($user = null) {
+        if($user) {
+            $field = (is_numeric($user)) ? 'id' : 'username';
+            $data = $this->_db->get('users', array($field, '=', $user));
+
+            if($data->count()) {
+                $this->_data = $data->first();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function login($username = null, $password = null) {
+        $user = $this->find($username);
+
+        if($user) {
+            if(password_verify(Input::get('password'), $this->data()->password)){
+                echo "OK";
+                Session :: put($this->_sessionName, $this->data()->id);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function data(){
+        return $this->_data;
     }
 }
